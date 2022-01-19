dd5ad0e6ffe1194886cda920c95070517409a64c
jfellows0 <jf671420@ohio.edu>
2022-01-18 19:04:12 -0500

    C++ Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jeremy Fellows
+ *        Date: January 18th
+ * Description: lab code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
